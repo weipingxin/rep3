991fac260b5ba16f0d30d2bb55462aad6aba036f
weipingxin <742735163@qq.com>
2020-05-06 15:39:29 +0800

    第二次提交

--- git/src/com/itheima/Demo.java
+++ git/src/com/itheima/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("123");
     }
 }
